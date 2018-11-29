9790cd895f886172b10bc5a3543c9d5e15d90115
起床啦！ <1106728884@qq.com>
2018-11-29 10:27:33 +0800

    引入必要组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import eleNav from './lib/';
+import ElementUI from 'element-ui';
 Vue.config.productionTip = false; // 设置为 false 以阻止 vue 在启动时生成生产提示
 
+Vue.use(ElementUI);
 Vue.use(eleNav);
 new Vue({
   el: '#app',
